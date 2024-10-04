d9d4267224c104dd7cd607b32be0ec8a49e7f5de
CarlosMartin2005 <cpenap@unah.hn>
2024-10-04 08:57:38 -0600

    Ejercicio 7

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,13 +1,3 @@
-# Escribe una función llamada fibonacci(n) que imprima los primeros n números de la
-# secuencia de Fibonacci.
-# Instrucciones:
-# 1. Implementa la función fibonacci(n).
-# 2. Pide al usuario que ingrese el valor de n.
-# 3. Llama a la función para mostrar la secuencia.
-# Ejemplo de salida:
-# Ingrese el número de términos: 7
-# Secuencia de Fibonacci:
-# 0, 1, 1, 2, 3, 5, 8
 
 def fibonacci(n):
     list = [0, 1]
@@ -19,8 +9,3 @@ def fibonacci(n):
 n = int(input("Ingrese el número de términos: "))
 print("Secuencia de Fibonacci:")
 print(*fibonacci(n), sep=", ")
-
-# Output:
-# Ingrese el número de términos: 7
-# Secuencia de Fibonacci:
-# 0, 1, 1, 2, 3, 5, 8
---
+++ ejercicio7.py
@@ -0,0 +1,5 @@
+x = int(input("Ingrese un número: "))
+print("Tabla de multiplicar del " , x)
+
+for i in range(1, 11):
+    print(f"{x} x {i} = {x * i}")
